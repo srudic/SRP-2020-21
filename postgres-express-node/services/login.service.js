cc138a7d4cc03ef0c1b7bf8d85a82c5a99547c49
Sanja Rudic <srudic00@fesb.hr>
2020-11-10 14:41:57 +0100

    Start refactoring login service

--- postgres-express-node/services/login.service.js
+++ postgres-express-node/services/login.service.js
@@ -1,3 +1,6 @@
+const jwt = require("jsonwebtoken");
+
+
 class LoginService {
   constructor({ logger, userModel }) {
     this.userModel = userModel;
@@ -12,6 +15,42 @@ class LoginService {
     });
     return user;
   }
+
+  async login(username, password){
+    const userRecord = await this.userModel.findOne({
+      where: { username },
+    });
+
+    if (!userRecord) {
+      this.logger.error("User not registered");
+      throw new Error("Authentication failed");
+    }
+    if (userRecord.password === password) {
+      this.logger.info("Password correct so proceed and generate a JWT");
+      
+      const user = {
+        username: userRecord.username,
+        role: userRecord.role || "guest",
+      };
+ 
+      const payload = {
+        ...user,
+        aud: config.jwt.audience || "localhost/api",
+        iss: config.jwt.issuer || "localhost@fesb",
+      };
+
+      const token = this.generateToken(payload);
+      return { user, token }
+
+    }
+  }
+
+  generateToken(payload) {
+    return jwt.sign(payload, config.jwt.secret, {
+      expiresIn: config.jwt.expiresIn,
+    });
+  }
+
 }
 
 module.exports = LoginService;
